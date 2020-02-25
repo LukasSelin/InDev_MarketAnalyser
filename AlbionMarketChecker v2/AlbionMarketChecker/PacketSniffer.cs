a673b68149dd10faf754881e72e7d621758c59b2
Lukas Selin <lukas.selin@viskan.se>
2020-02-25 12:10:58 +0100

    Cleaned up the code

---
+++ AlbionMarketChecker v2/AlbionMarketChecker/PacketSniffer.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using PcapDotNet.Core;
+using PcapDotNet.Packets;
+using menu;
+
+namespace AlbionMarketChecker
+{
+    public static class PacketSniffer
+    {
+        public static string filterString = "ip and udp and len == 1242";
+        public static LivePacketDevice SelectDevice()
+        {
+            IList<LivePacketDevice> allDevcies = LivePacketDevice.AllLocalMachine;
+
+            if (allDevcies.Count == 0)
+            {
+                Console.WriteLine("No interfaces found.");
+            }
+
+            string menuHeader = "Select a capture device";
+            List<string> menuContent = new List<string>();
+
+            foreach (var device in allDevcies)
+            {
+                menuContent.Add(device.Description);
+            }
+
+            var menu = new Menu(menuContent);
+            menu = menu.GetMenu(menu, menuHeader);
+
+            return allDevcies[menu.SelectedIndex];
+        } // Select device
+
+        public static List<string> ListenFromDevice(LivePacketDevice device)
+        {
+            using (PacketCommunicator communicator =
+                    device.Open(62388, PacketDeviceOpenAttributes.Promiscuous, 1000))
+            {
+                using (BerkeleyPacketFilter filter = communicator.CreateFilter(filterString))
+                {
+                    communicator.SetFilter(filter);
+                }
+
+                Console.WriteLine("Listening on " + device.Description + "...");
+
+                communicator.ReceivePackets(0, PacketHandler);
+                return auctionList;
+            }
+        }
+
+        private static string uncompletedAuction;
+        private static List<string> auctionList = new List<string>();
+        private static void PacketHandler(Packet packet)
+        {
+            var convertedString = packet.Ethernet.IpV4.Udp.Payload.Decode(Encoding.ASCII);
+            bool isTrue = true;
+
+            int beginingOfAuction = convertedString.IndexOf('{');
+            string auction = convertedString.Substring(beginingOfAuction);
+
+            do
+            {
+                if (uncompletedAuction == null)
+                {
+                    int endOfAuctionIndex = auction.IndexOf('}');
+                    if (endOfAuctionIndex == -1)
+                    {
+                        uncompletedAuction = auction;
+                        isTrue = false;
+                    }
+                    else
+                    {
+                        string completedAuction = auction.Substring(0, endOfAuctionIndex + 1);
+                        auctionList.Add(completedAuction);
+
+                        // Get remaining data from the packet
+                        auction = auction.Substring(endOfAuctionIndex);
+                    }
+                }
+                else
+                {
+                    var payload = packet.Ethernet.IpV4.Udp.Payload;
+                    var splitAuction = payload.Subsegment(44, payload.Decode(Encoding.ASCII).IndexOf('}'));
+                    var completedString = uncompletedAuction + splitAuction;
+
+                    uncompletedAuction = null;
+                    auctionList.Add(completedString);
+                }
+            } while (isTrue);
+        }
+    }
+}
--- AlbionMarketChecker v2/AlbionMarketChecker/Program.cs
+++ AlbionMarketChecker v2/AlbionMarketChecker/Program.cs
@@ -1,11 +1,6 @@
-﻿using menu;
-using PcapDotNet.Core;
-using PcapDotNet.Packets;
-using System;
+﻿using System;
 using System.Text;
-
 using System.Collections.Generic;
-
 
 
 namespace AlbionMarketChecker
@@ -14,98 +9,8 @@ namespace AlbionMarketChecker
     {
         static void Main(string[] args)
         {
-            SelectDevice();
-        }
-
-        static void SelectDevice()
-        {
-
-            IList<LivePacketDevice> allDevcies = LivePacketDevice.AllLocalMachine;
-
-            if (allDevcies.Count == 0)
-            {
-                Console.WriteLine("No interfaces found.");
-            }
-
-            string menuHeader = "Select capture device";
-            List<string> menuContent = new List<string>();
-
-            foreach (var device in allDevcies)
-            {
-                menuContent.Add(device.Description);
-            }
-            var menu = new Menu(menuContent);
-            menu = menu.GetMenu(menu, menuHeader);
-
-            PacketDevice selectedDevice = allDevcies[menu.SelectedIndex];
-
-            using (PacketCommunicator communicator =
-                    selectedDevice.Open(62388, PacketDeviceOpenAttributes.Promiscuous, 1000))
-            {
-                using (BerkeleyPacketFilter filter = communicator.CreateFilter("ip and udp and len == 1242"))
-                {
-                    communicator.SetFilter(filter);
-                }
-
-
-
-                Console.WriteLine("Listening on " + selectedDevice.Description + "...");
-
-                communicator.ReceivePackets(0, PacketHandler);
-            }
-        } // Select device
-
-        static string uncompletedAuction;
-        static List<string> vs = new List<string>();
-        private static void PacketHandler(Packet packet)
-        {
-            // Console.WriteLine(packet.Timestamp.ToString("yyyy-MM-dd") + " length:" + packet.Length);
-            // The data payload
-            // packet.Ethernet.IpV4.Udp.Payload
-            var convertedString = packet.Ethernet.IpV4.Udp.Payload.Decode(Encoding.ASCII);
-            bool isTrue = true;
-            string auction;
-            int beginingOfAuction = convertedString.IndexOf('{');
-            auction = convertedString.Substring(beginingOfAuction);
-
-            do
-            {
-                if(uncompletedAuction == null)
-                {
-
-                    if (auction.Contains('}'))
-                    {
-                       //  uncompletedAuction = null;
-                        int endOfAuction = auction.IndexOf('}') + 1;
-                        string completedAuction = auction.Substring(0, endOfAuction);
-                        Console.WriteLine(completedAuction);
-                        vs.Add(completedAuction);
-
-                        // Send CompletedAuction to the database
-
-                        // Get remaining data from the packet
-                        auction = auction.Substring(endOfAuction);
-
-                    }
-                    else
-                    {
-                        uncompletedAuction = auction;
-                        isTrue = false;
-                        Console.WriteLine("Wohooo äntligen här!");
-                    }
-                }
-                else
-                {
-                    // 44 bytes skippa i början alltid (tror jag)
-                    var middleAuctionString = packet.Ethernet.IpV4.Udp.Payload.Subsegment(44, packet.Ethernet.IpV4.Udp.Payload.Length - 44);
-                    var completedString = uncompletedAuction + convertedString;
-
-                    // Send CompletedAuction to the database
-                    Console.WriteLine(completedString);
-                    vs.Add(completedString);
-                    uncompletedAuction = null;
-                }
-            } while (isTrue);
+            List<string> auctionList = new List<string>();
+            auctionList = PacketSniffer.ListenFromDevice(PacketSniffer.SelectDevice());
         }
     }
 }
